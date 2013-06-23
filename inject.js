8729f9bf509cfa2ba9602eeff5da60f1c862e956
Steven Lee <lee0741@gmail.com>
2013-06-23 17:59:56 +0800

    when isbn search goes wrong

--- inject.js
+++ inject.js
@@ -1,19 +1,25 @@
+var isbn = $('#info').text().split('ISBN:')[1].split(' ')[1],
+    title = $('h1 span:first').text(),
+    isbnurl = 'http://hbtinterlib.library.hb.cn/opac/search?rows=10&searchWay=isbn&q=' + isbn,
+    titleurl = 'http://hbtinterlib.library.hb.cn/opac/search?rows=10&searchWay=title&q=' + title;
+
 $('#buyinfo').before('<div class="gray_ad" id="hblib"></div>');
 $('#hblib').append('<h2>湖北省图书馆有没有该书?</h2><div class="bs" id="isex"></div>');
+
 if (typeof($('#info').text().split('ISBN:')[1]) != 'undefined') {
-  var isbn = $('#info').text().split('ISBN:')[1].split(' ')[1];
   $.ajax({
-    url: 'http://hbtinterlib.library.hb.cn/opac/search?rows=10&searchWay=isbn&q=' + isbn,
+    url: isbnurl,
 	  success: function (msg) {
       if (msg.indexOf('检索不到记录!') != -1) {
 		    $('#isex').html('看起来湖北省图书馆还没有该书。');
+        $('#hblib').append('<div class="bs" id="mdt"><a href="' + titleurl + '">建议查询</a></div>');
 	    } else {
 		    $('#isex').html('湖北省图书馆当然有！');
-		    var findurl = 'http://hbtinterlib.library.hb.cn/opac/search?rows=10&searchWay=isbn&q=' + isbn;
-		    $('#hblib').append('<div class="bs" id="mdt"><a href="' + findurl + '" target="_blank">具体详情点这里</a></div>');
+		    $('#hblib').append('<div class="bs" id="mdt"><a href="' + isbnurl + '">具体详情</a></div>');
 		  }
 		}
   });
 } else {
-	$('#isex').html('竟然没有！');
+	$('#isex').html('由于该书在豆瓣上的信息不全导致查询失败。');
+  $('#hblib').append('<div class="bs" id="mdt"><a href="' + titleurl + '">建议查询</a></div>');
 }
