7173754b6046133ab18d9c7806f41ecc3dd2f162
Steven Lee <lee0741@gmail.com>
2013-07-20 06:11:29 +0800

    adding preview

--- inject.js
+++ inject.js
@@ -4,22 +4,66 @@ var isbn = $('#info').text().split('ISBN:')[1].split(' ')[1],
     titleurl = 'http://hbtinterlib.library.hb.cn/opac/search?rows=10&searchWay=title&q=' + title;
 
 $('#buyinfo').before('<div class="gray_ad" id="hblib"></div>');
-$('#hblib').append('<h2>湖北省图书馆有没有该书?</h2><div class="bs" id="isex"></div>');
+$('#hblib').append('<h2>湖北省图书馆有没有该书?</h2><div class="bs pl" id="hbinfo"></div>');
 
 if (typeof($('#info').text().split('ISBN:')[1]) != 'undefined') {
   $.ajax({
     url: isbnurl,
 	  success: function (msg) {
       if (msg.indexOf('检索不到记录!') != -1) {
-		    $('#isex').html('看起来湖北省图书馆还没有该书。');
-        $('#hblib').append('<div class="bs" id="mdt"><a href="' + titleurl + '">建议查询</a></div>');
+		    $('#hbinfo').html('看起来湖北省图书馆还没有该书。');
+        $('#hblib').append('<div style="margin-top:8px;"><a href="' + titleurl + '">建议查询</a></div>');
 	    } else {
-		    $('#isex').html('湖北省图书馆当然有！');
-		    $('#hblib').append('<div class="bs" id="mdt"><a href="' + isbnurl + '">具体详情</a></div>');
+        var bookrecno = $(msg).find('.expressServiceTab:first').attr('express_bookrecno');
+        $.ajax({
+          url: 'http://hbtinterlib.library.hb.cn/opac/book/holdingpreview/' + bookrecno,
+          data: {},
+          dataType: 'xml',
+          success: function (xml) {
+            var tableHeader = new Array('索书号', '所在馆', '所在馆藏地点', '在馆复本数'),
+                tableContent = new Array(),
+                records = $(xml).find("record"),
+                tableStr = '';
+            
+            function createTable(tableHeader, tableContent) {
+              var s = '<table><tr>';
+              for (index in tableHeader) {
+                s += '<td style="padding:3px;text-align:center;border:1px dashed #ddd;"><b>' + tableHeader[index] + '</b></td>';
+              }
+              s += '</tr>';
+              for (index in tableContent) {
+                s += '<tr>';
+                var recordData = tableContent[index];
+                for (j in recordData) {
+                  s += '<td style="padding:3px;text-align:center;border:1px dashed #ddd;">' + recordData[j] + '</td>';
+                }
+                s += '</tr>';
+              }
+              s += '</tableContentleable>';
+              return s;
+            }  
+
+            records.each(function(i) {
+              tableContent.push([$(this).find('callno').text(),
+                                 $(this).find('curlibName').text(), 
+                                 $(this).find('curlocalName').text(),
+                                 $(this).find('copycount').text()]);
+            });
+
+            if (records.length == 0) {
+              tableStr = '湖北省图书馆有该书，但暂时没有在馆复本记录。';
+            } else {
+              tableStr = createTable(tableHeader, tableContent);
+            }
+
+            $('#hbinfo').html(tableStr);
+            $('#hblib').append('<div style="margin-top:8px;"><a href="' + isbnurl + '">具体详情</a></div>');
+          }
+        });
 		  }
 		}
   });
 } else {
-	$('#isex').html('由于该书在豆瓣上的信息不全导致查询失败。');
-  $('#hblib').append('<div class="bs" id="mdt"><a href="' + titleurl + '">建议查询</a></div>');
+	$('#hbinfo').html('由于该书在豆瓣上的信息不全导致查询失败。');
+  $('#hblib').append('<div style="margin-top:8px;"><a href="' + titleurl + '">建议查询</a></div>');
 }
